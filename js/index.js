088810b08f71da603a4032ff9f5650ecf5252d31
yashvi2737 <yashvigajjar2737@gmail.com>
2026-01-30 10:10:32 -0600

    Revert "registration form and org. profile page"
    This reverts commit d95ac3e88b210a6b8d2d81e0c63ab314e4ef806c.
    
    t

--- js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.getElementById("btn-AI-search");
-
-    btn.addEventListener("click", async function() {
-        const prompt = document.getElementById("txtAIPrompt").value;
-        sessionStorage.setItem("user_prompt", prompt); // Store prompt
-        window.location.href = "../../html/pages/ai_results.html"; // carries to ai_results page
-    });
-});
