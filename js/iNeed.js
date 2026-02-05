52456dd50eeaa9b17167dde2bbd4f5a3324969e2
shlprtr <158105498+shlprtr@users.noreply.github.com>
2026-02-05 16:01:48 -0600

    making the emails work for ashley's email

---
+++ js/iNeed.js
@@ -0,0 +1,46 @@
+document.ddEventListener("DOMContentLoaded", function() {
+    //array of chosen services for food
+    arrFoodServiceIDs = []
+    //
+})
+
+
+
+// "I need" cards
+//<div class="p-5 " style="margin: 10px;">
+//<div id="wrapperHumanServicesHumanMinds" class="pt-5 d-block">
+//    <div id="frmHumanServicesHumanMind" class="row justify-content-center">
+//        <div class="card m-2 col-12 col-md-5" style="width: 18rem;">
+//            <img src="\assets\images\placeholder-img.webp" class="card-img-top pt-3" alt="...">
+//            <div class="card-body">
+//                <h5 class="card-title">Card title</h5>
+//                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                <a href="#" class="btn btn-primary">Go somewhere</a>
+//            </div>
+//        </div>
+//        <div class="card m-2 col-12 col-md-5" style="width: 18rem;">
+//            <img src="\assets\images\placeholder-img.webp" class="card-img-top pt-3" alt="...">
+//            <div class="card-body">
+//                <h5 class="card-title">Card title</h5>
+//                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                <a href="#" class="btn btn-primary">Go somewhere</a>
+//            </div>
+//        </div>
+//        <div class="card m-2 col-12 col-md-5" style="width: 18rem;">
+//            <img src="\assets\images\placeholder-img.webp" class="card-img-top pt-3" alt="...">
+//            <div class="card-body">
+//                <h5 class="card-title">Card title</h5>
+//                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                <a href="#" class="btn btn-primary">Go somewhere</a>
+//            </div>
+//        </div>
+//        <div class="card m-2 col-12 col-md-5" style="width: 18rem;">
+//            <img src="\assets\images\placeholder-img.webp" class="card-img-top pt-3" alt="...">
+//            <div class="card-body">
+//                <h5 class="card-title">Card title</h5>
+//                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                <a href="#" class="btn btn-primary">Go somewhere</a>
+//            </div>
+//        </div>
+//    </div>
+//</div>
